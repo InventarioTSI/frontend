dd4d5c5267bcdbed63b7cbe9777e8abfc7c873e4
GDARKKING <tomasp@Portatil-50-2.tsisl.dom>
2025-03-25 14:31:16 +0100

    Cambios visuales en home y qrCam

--- src/pages/Home/Body/Body.jsx
+++ src/pages/Home/Body/Body.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./Body.css";
-import LectorQR from './LectorQR/LectorQR'
-import GestionPersonal from './GestionPersonal/GentionPeronsal';
-import ListaEquipos from './ListaEquipos/ListaEquipos';
-import NuevoEquipo from './NuevoEquipo/NuevoEquipo'; 
+import LectorQR from "./LectorQR/LectorQR";
+import GestionPersonal from "./GestionPersonal/GestionPersonal";
+import ListaEquipos from "./ListaEquipos/ListaEquipos";
+import NuevoEquipo from "./NuevoEquipo/NuevoEquipo";
 
 const Body = () => {
-    return (
-        <div className='contenedor'>
-            <div className="fila">
-                <div className='div-div'><LectorQR/></div>
-                <div className='div-div'><ListaEquipos/></div>
-            </div>
-            <div className="fila">
-                <div className='div-div'><NuevoEquipo/></div>
-                <div className='div-div'><GestionPersonal/></div>
-            </div>
+  return (
+    <div className="contenedor">
+      <div className="fila">
+        <div className="div-div">
+          <LectorQR />
         </div>
-    );
-}
+        <div className="div-div">
+          <ListaEquipos />
+        </div>
+      </div>
+      <div className="fila">
+        <div className="div-div">
+          <NuevoEquipo />
+        </div>
+        <div className="div-div">
+          <GestionPersonal />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Body;
---
+++ src/pages/Home/Body/GestionPersonal/GestionPersonal.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./GestionPersonal.css";
+import Personal from "../../../../imagenes/Personal.jpg";
+
+const GestionPersonal = () => {
+  return (
+    <div className="divPersonal">
+      <Link to="/Personal" className="Link">
+        <a className="BotonPersonal">
+          <img
+            className="BotonPersonalImage"
+            src={Personal}
+            alt="Texto alternativo de la imagen"
+          />
+          <p className="BotonPersonalText">Gestiona el personal</p>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default GestionPersonal;
