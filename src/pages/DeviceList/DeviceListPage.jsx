e76a540b40c7f89bd01e0f7da83349bc8ad284a3
unknown <andresj@Portatil-52-2.tsisl.dom>
2025-03-26 16:41:39 +0100

    Creacion de tabla de usuarios

--- src/pages/DeviceList/DeviceListPage.jsx
+++ src/pages/DeviceList/DeviceListPage.jsx
@@ -2,7 +2,7 @@ import Pagination from "../../components/Pagination/Pagination";
 import Template from "../../components/Template";
 import { useDevice } from "../../context/DeviceContext";
 import { useEffect, useState, useRef } from "react";
-import { Link, useNavigate } from "react-router-dom"; // Importamos useNavigate
+import { Link, useNavigate } from "react-router-dom"; 
 import "./DeviceListPage.css";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import axios from "axios"; // Importamos axios para manejar cancelación de solicitudes
