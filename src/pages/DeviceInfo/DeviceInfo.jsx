6485819aa6c94311c134b0457bbf924cb855bcca
GDARKKING <tomasp@Portatil-50-2.tsisl.dom>
2025-03-31 13:16:48 +0200

    cambios para actuelizar faltan informes

--- src/pages/DeviceInfo/DeviceInfo.jsx
+++ src/pages/DeviceInfo/DeviceInfo.jsx
@@ -3,18 +3,13 @@ import { useDevice } from "../../context/DeviceContext";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import {
-  getDeviceHistoricRequest,
-  createHistoricRequest,
-} from "../../api/historic";
 import "./DeviceInfo.css";
 
 function DeviceInfo() {
   const { deviceId, deviceType } = useParams();
   const [deviceData, setDeviceData] = useState(null);
-  const [deviceHistoric, setDeviceHistoric] = useState([]);
-  const [employees, setEmployees] = useState([]);
-  const [isViewMode, setIsViewMode] = useState(true); // Modo "Ver"
+  const [isEditMode, setIsEditMode] = useState(false); // Controla si el formulario de edición está abierto
+  const [isModalOpen, setIsModalOpen] = useState(false); // Controla si el modal del informe está abierto
   const navigate = useNavigate();
   const { user } = useAuth();
 
@@ -31,24 +26,15 @@ function DeviceInfo() {
     updateDevice,
     deleteDevice,
     errors: responseErrors,
-    getAllDevices,
   } = useDevice();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setDeviceData(null);
-        setDeviceHistoric([]);
 
-        const [historic, device, devicesList] = await Promise.all([
-          getDeviceHistoricRequest(deviceId),
-          getOneDevice(deviceType, deviceId),
-          getAllDevices(1, 10, "", "", ""),
-        ]);
-
-        setDeviceHistoric(historic.data.data);
+        const device = await getOneDevice(deviceType, deviceId);
         setDeviceData(device);
-        setEmployees(devicesList.employees);
 
         device.deviceFields.forEach((field) => {
           setValue(field.name, field.value);
@@ -65,8 +51,49 @@ function DeviceInfo() {
     };
   }, [deviceType, deviceId]);
 
+  const handleUpdate = handleSubmit(async (data) => {
+    try {
+      const relevantData = Object.keys(data)
+        .filter((key) =>
+          deviceData.deviceFields.some((field) => field.name === key)
+        )
+        .reduce((obj, key) => {
+          obj[key] = data[key];
+          return obj;
+        }, {});
+
+      await updateDevice(deviceType, deviceId, relevantData);
+      alert("¡Dispositivo actualizado con éxito!");
+      setIsEditMode(false); // Cierra el formulario de edición
+      const updatedDevice = await getOneDevice(deviceType, deviceId);
+      setDeviceData(updatedDevice);
+    } catch (error) {
+      console.error("Error updating device:", error);
+    }
+  });
+
+  const handleDelete = async () => {
+    if (
+      window.confirm("¿Estás seguro de que quieres eliminar este dispositivo?")
+    ) {
+      try {
+        await deleteDevice(deviceType, deviceId);
+        alert("¡Dispositivo eliminado con éxito!");
+        navigate("/DeviceList", { replace: true });
+      } catch (error) {
+        console.error("Error deleting device:", error);
+        alert("Hubo un problema al eliminar el dispositivo.");
+      }
+    }
+  };
+
   const handleNavigateHome = () => {
     navigate("/home", { replace: true });
+  };
+
+  const handleGenerateReport = () => {
+    // Aquí puedes implementar la lógica para generar el informe
+    alert("Generando informe...");
   };
 
   if (!deviceData) return <div>Loading...</div>;
@@ -84,14 +111,14 @@ function DeviceInfo() {
       )}
       <div className="titulo-device">Tipo de Dispositivo: {deviceType}</div>
 
-      {isViewMode ? (
-        // Modo "Ver": Mostrar datos en una tabla dividida en dos columnas
+      {!isEditMode ? (
+        // Vista de solo lectura
         <div className="view-mode">
           <h1>Detalles del dispositivo</h1>
           <div className="device-details-table">
             <div className="table-column">
               {filteredFields
-                .filter((_, index) => index % 2 === 0) // Primera columna: campos pares
+                .filter((_, index) => index % 2 === 0)
                 .map((field) => (
                   <div key={field.name} className="row">
                     <div className="cell">{field.name}</div>
@@ -101,7 +128,7 @@ function DeviceInfo() {
             </div>
             <div className="table-column">
               {filteredFields
-                .filter((_, index) => index % 2 !== 0) // Segunda columna: campos impares
+                .filter((_, index) => index % 2 !== 0)
                 .map((field) => (
                   <div key={field.name} className="row">
                     <div className="cell">{field.name}</div>
@@ -110,13 +137,29 @@ function DeviceInfo() {
                 ))}
             </div>
           </div>
-          <button className="cancel-button" onClick={handleNavigateHome}>
-            Volver al menú
-          </button>
+          <div className="form-buttons">
+            <button className="cancel-button" onClick={handleNavigateHome}>
+              Volver al menú
+            </button>
+            {user.role === "Admin" && (
+              <>
+                <button
+                  className="submit-button"
+                  onClick={() => setIsEditMode(true)}
+                >
+                  Actualizar
+                </button>
+                <button className="delete-button" onClick={handleDelete}>
+                  Eliminar
+                </button>
+              </>
+            )}
+          </div>
         </div>
       ) : (
-        // Modo "Formulario": Mostrar campos de formulario
-        <form onSubmit={handleSubmit(async (data) => {})} className="form">
+        // Formulario de edición
+        <form onSubmit={handleUpdate} className="form">
+          <h1>Editar dispositivo</h1>
           <div className="form-fields">
             <div className="column">
               {filteredFields
@@ -136,9 +179,37 @@ function DeviceInfo() {
                       </label>
                     </div>
                     <div className="cell">
-                      {errors[field.name] && (
-                        <p className="error-text">Este campo es requerido</p>
-                      )}
+                      <input
+                        className={`form-input ${
+                          errors[field.name] ? "error" : ""
+                        }`}
+                        type="text"
+                        name={field.name}
+                        defaultValue={field.value}
+                        {...register(field.name)}
+                      />
+                    </div>
+                  </div>
+                ))}
+            </div>
+            <div className="column">
+              {filteredFields
+                .filter((_, index) => index % 2 !== 0)
+                .map((field) => (
+                  <div
+                    key={field.name}
+                    className={`row ${errors[field.name] ? "error" : ""}`}
+                  >
+                    <div className="cell">
+                      <label
+                        className={`form-label ${
+                          errors[field.name] ? "error" : ""
+                        }`}
+                      >
+                        {field.name}
+                      </label>
+                    </div>
+                    <div className="cell">
                       <input
                         className={`form-input ${
                           errors[field.name] ? "error" : ""
@@ -157,13 +228,23 @@ function DeviceInfo() {
             <button
               type="button"
               className="cancel-button"
-              onClick={handleNavigateHome}
+              onClick={() => setIsEditMode(false)}
             >
-              Volver al menú
+              Cancelar
+            </button>
+            <button type="submit" className="submit-button">
+              Guardar cambios
             </button>
           </div>
         </form>
       )}
+
+      {/* Botón para generar informe */}
+      <div className="form-buttons">
+        <button className="report-button" onClick={handleGenerateReport}>
+          Generar Informe
+        </button>
+      </div>
     </div>
   );
 }
