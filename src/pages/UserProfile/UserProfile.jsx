1a4ebc8b31784430a1918751fec9da16a4237287
GDARKKING <tomasp@Portatil-50-2.tsisl.dom>
2025-03-31 10:37:09 +0200

    cambios perfil usuario

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import "./UserProfile.css";
 
 const UserProfile = () => {
+  const { user } = useAuth(); // Obtener el usuario autenticado del contexto
+
   return (
     <div className="user-profile">
-      <h1>Perfil de Usuario</h1>
-      <p>
-        Este es el componente de perfil. Aquí se mostrarán los datos del
-        usuario.
-      </p>
+      <h1 className="titulo">Perfil de Usuario</h1>
+      <table className="profile-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Rol</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{user.id}</td>
+            <td>{user.userName}</td>
+            <td>{user.role}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button className="boton-cancelar" onClick={() => window.history.back()}>
+        Volver
+      </button>
     </div>
   );
 };
